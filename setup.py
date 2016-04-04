01ef2967e5740b0d47aa871be162a4bfe9d11b78
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2016-04-03 23:54:42 -0300

    Commit to force Coveralls to build

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
 from distutils.core import setup
 
-
 classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
